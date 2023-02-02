10c5c0471b97875659c07242adf109cff73f1680
hiltpold <m.hiltpold@protonmail.ch>
2023-02-02 08:14:48 +0100

    docker scripts

--- yolov7/scripts/militarycrafts_train_detached.sh
+++ yolov7/scripts/militarycrafts_train_detached.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-RUN_NAME=train_yolov7_tiny
-cd /usr/src/app
-
+RUN_NAME=militaryaircrafts_yolov7_custom
 docker run --gpus all \
+	   --user=$(id -u):$(id -g) \
            --shm-size=256g \
 	   --rm \
-	   -d \
-	   --user=$(id -u):$(id -g) \
+	   -dit \
 	   --volume /home/mhp/yolov7/dataset/:/usr/src/app/external/dataset \
 	   --volume /home/mhp/yolov7/data:/usr/src/app/external/data \
 	   --volume /home/mhp/yolov7/cfg:/usr/src/app/external/cfg \
 	   --volume /home/mhp/yolov7/model/:/usr/src/app/external/model \
 	   --volume /home/mhp/yolov7/scripts/:/usr/src/app/external/scripts \
 	   prj-elca-bl9product-mlops-docker.artifactory.svc.elca.ch/yolov7:mhp python train.py \
-           --data external/data/militaryaircraft.yaml \
+           --data external/data/militaryaircrafts.yaml \
            --hyp external/data/hyp.scratch.custom.yaml \
            --cfg external/cfg/yolov7custom.yaml \
            --weights external/model/yolov7custom.pt \
--- yolov7/scripts/run_yolov7.sh
+++ yolov7/scripts/run_yolov7.sh
@@ -6,4 +6,5 @@ docker run --gpus all \
 	   --volume /home/mhp/yolov7/cfg:/usr/src/app/external/cfg \
 	   --volume /home/mhp/yolov7/model/:/usr/src/app/external/model \
 	   --volume /home/mhp/yolov7/scripts/:/usr/src/app/external/scripts \
+	   --volume /home/mhp/yolov7/inference/:/usr/src/app/external/inference \
 	   prj-elca-bl9product-mlops-docker.artifactory.svc.elca.ch/yolov7:mhp
